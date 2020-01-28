c4ab600f479823b2480321f2c5bc25b77b0a83c9
Rodrigo <rodrigor.gomes@hotmail.com>
2020-01-28 00:12:28 -0300

    First version

---
+++ SQLAD/CreateDomain.ps1
@@ -0,0 +1,61 @@
+param($DomainName = $Env:AD_DOMAIN_NAME, $SafePassword = $Env:AD_SAFE_PASSWORD)
+
+$ErrorActionPreference = "Stop";
+$LogFile = "$PsScriptRoot.log"
+
+function log {
+    $ts 	= (Get-Date).ToString("yyyy-MM-SS HH:mm:ss")
+    $m	= "$ts " + ($Args -Join " ");
+
+    
+    $m | tee-object $LogFile
+}
+
+log "Script started at $(Get-Date)..."
+
+try {
+    log "Domain: $DomainName"
+
+    if(!$DomainName){
+        throw "INVALID_DOMAIN: EMPTY_DOMAIN"
+    }
+
+    #Get the first part!
+    $DomainParts = $DomainName.split('.');
+
+    if($DomainParts.length -eq 1){
+        throw "INVALID_DOMAIN: TOP_LEVEL_NOTSUPPORTED: $DomainName"
+    }
+
+    $DomainNetBios = $DomainParts[0];
+    log "   DomainNetBios: $DomainNetBios".
+
+
+    log "Installing window feature AD-Domain-Services"
+    Install-WindowsFeature AD-Domain-Services
+
+    log "Importing ad deployment module"
+    Import-Module ADDSDeployment
+
+    $SecureStringSafePassword = ConvertTo-SecureString -String $SafePassword -AsPlainText -Force;
+
+    $AdForestParams = @{
+        CreateDnsDelegation     = $true
+        DatabasePath            = 'C:\Windows\NTDS'
+        DomainMode              = 'Win2012R2'
+        DomainName              = $DomainName
+        DomainNetbiosName       = $DomainNetBios
+        ForestMode              = "Win2012R2"
+        InstallDns              = $true
+        LogPath                 = 'C:\Windows\NTDS'
+        NoRebootOnCompletion    = $false
+        SysvolPath              = 'C:\Windows\SYSVOL'
+        Force                   = $true
+        SafeModeAdministratorPassword = $SecureStringSafePassword
+    }
+
+    Log "Installing forest...";
+    Install-ADDSForest @AdForestParams;
+} catch {
+    log "Fail: $_";
+}
